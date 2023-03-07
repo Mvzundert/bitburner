6f2448992dd67c676695cde393902991d029c6ed
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-03-07 11:46:04 +0100

    Added early game scripts

---
+++ scripts/restart.js
@@ -0,0 +1,45 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    // Array of all servers that don't need any ports opened
+    // to gain root access. These have 16 GB of RAM
+    const servers0Port = ["sigma-cosmetics",
+                        "joesguns",
+                        "nectar-net",
+                        "hong-fang-tea",
+                        "harakiri-sushi"];
+
+    // Array of all servers that only need 1 port opened
+    // to gain root access. These have 32 GB of RAM
+    const servers1Port = ["neo-net",
+                        "zer0",
+                        "max-hardware",
+                        "iron-gym"];
+
+    // Copy our scripts onto each server that requires 0 ports
+    // to gain root access. Then use nuke() to gain admin access and
+    // run the scripts.
+    for (let i = 0; i < servers0Port.length; ++i) {
+        const serv = servers0Port[i];
+
+        ns.scp("early-hack-template.js", serv);
+        ns.nuke(serv);
+        ns.exec("early-hack-template.js", serv, 6);
+    }
+
+    // Wait until we acquire the "BruteSSH.exe" program
+    while (!ns.fileExists("BruteSSH.exe")) {
+        await ns.sleep(60000);
+    }
+
+    // Copy our scripts onto each server that requires 1 port
+    // to gain root access. Then use brutessh() and nuke()
+    // to gain admin access and run the scripts.
+    for (let i = 0; i < servers1Port.length; ++i) {
+        const serv = servers1Port[i];
+
+        ns.scp("early-hack-template.js", serv);
+        ns.brutessh(serv);
+        ns.nuke(serv);
+        ns.exec("early-hack-template.sj", serv, 12);
+    }
+}
